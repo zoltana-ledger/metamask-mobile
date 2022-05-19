c45be9a4ebca7d710ec373266324e5cf4525f7da
Mike B <michael.berardi@consensys.net>
2022-05-19 16:36:37 -0400

    format change again

--- app/core/PreventScreenshot.js
+++ app/core/PreventScreenshot.js
@@ -1,13 +1,8 @@
-import { NativeModules, Platform } from 'react-native';
-
-const isAndroid = Platform.OS === 'android';
-
 export default {
-forbid: () => {
-  true;
-  },
-allow: () => {
-  true;
-  },
-};
-  
+    forbid: () => {
+      true;
+    },
+    allow: () => {
+      true;
+    },
+  };
