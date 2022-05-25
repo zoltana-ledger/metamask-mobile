f8265ef7d33dd0e32047a4ca798e6ca5963c2369
Plasma Corral <32695229+plasmacorral@users.noreply.github.com>
2022-05-25 15:38:34 -0400

    format change

--- app/core/PreventScreenshot.js
+++ app/core/PreventScreenshot.js
@@ -1,4 +1,8 @@
 export default {
-  forbid: isAndroid ? NativeModules.PreventScreenshot.forbid : () =>{ return true},
-  allow: isAndroid ? NativeModules.PreventScreenshot.allow : () =>{ return true}
+  forbid: () => {
+    true;
+  },
+  allow: () => {
+    true;
+  },
 };
