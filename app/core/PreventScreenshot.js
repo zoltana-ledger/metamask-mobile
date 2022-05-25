705bd3ad570af55710211c2577ae76c31cc66a9d
Plasma Corral <32695229+plasmacorral@users.noreply.github.com>
2022-05-25 15:22:48 -0400

    enable screenshots

--- app/core/PreventScreenshot.js
+++ app/core/PreventScreenshot.js
@@ -1,8 +1,4 @@
-import { NativeModules, Platform } from 'react-native';
-
-const isAndroid = Platform.OS === 'android';
-
 export default {
-  forbid: isAndroid ? NativeModules.PreventScreenshot.forbid : () => true,
-  allow: isAndroid ? NativeModules.PreventScreenshot.allow : () => true,
+  forbid: isAndroid ? NativeModules.PreventScreenshot.forbid : () =>{ return true},
+  allow: isAndroid ? NativeModules.PreventScreenshot.allow : () =>{ return true}
 };
